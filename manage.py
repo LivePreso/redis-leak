12076f17bcba2fc1253418bd2d701fd428c8a5d0
Ben Graham <bgraham@bgraham.com.au>
2021-11-03 17:08:08 +1100

    remember to monkeypatch

--- manage.py
+++ manage.py
@@ -2,6 +2,9 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import gevent.monkey
+
+gevent.monkey.patch_all()
 
 
 def main():
--- redis_leak/celery.py
+++ redis_leak/celery.py
@@ -1,3 +1,6 @@
+import gevent.monkey
+
+gevent.monkey.patch_all()
 import os
 
 from celery import Celery
